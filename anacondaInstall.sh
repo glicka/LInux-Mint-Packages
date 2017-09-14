11e2fa533d1c91791c292ec092f2c49f5dfc9c08
Adam Glick <adam_glick@berkeley.edu>
2017-09-14 13:06:27 -0700

    Mint Packages Install

---
+++ anacondaInstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# conda install script
+wget -c https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+bash Anaconda3-4.4.0-Linux-x86_64.sh
+conda install install gcc theano
