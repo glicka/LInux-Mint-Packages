6718a9dbe6c03572a0fe33db2876cc486af7c666
Adam Glick <adam_glick@berkeley.edu>
2018-06-04 12:45:26 -0700

    Added new install scripts for Linux Mint.

---
+++ dropboxInstall.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+~/.dropbox-dist/dropboxd
---
+++ slackInstall.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo apt-get install snap
+sudo snap install slack --classic
