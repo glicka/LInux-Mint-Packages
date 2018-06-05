e6af8fb80589d4efd8ee21aa1b2318d62e251873
Adam Glick <adam_glick@berkeley.edu>
2018-06-04 17:35:53 -0700

    Etcher Install Added

---
+++ etcherInstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd Desktop
+wget https://github.com/resin-io/etcher/releases/download/v1.4.4/etcher-electron-1.4.4-linux-x64.zip
+unzip etcher-electron-1.4.4-linux-x64.zip 
+rm -rf etcher-electron-1.4.4-linux-x64.zip 
+
