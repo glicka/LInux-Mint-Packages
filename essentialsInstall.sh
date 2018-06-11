9d63404dcb28da2d71cdd571e0578972eeeed400
Adam Glick <adam_glick@berkeley.edu>
2018-06-11 16:04:07 -0700

    essential packages for running linux smoothly added

---
+++ essentialsInstall.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#gedit
+sudo apt-get install gedit
+
+#cpp compiler
+sudo apt-get install build-essential
+
+#python essentials
+sudo apt-get install python-dev
+
+#pip
+sudo apt-get install python-pip
+
+#relevant python packages
+sudo pip install numpy
+sudo pip install scipy
+python -mpip install -U pip
+sudo python -mpip install -U matplotlib
